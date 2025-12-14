584068cab0b23bab7976b0c4192d17bfceeaa174
suokaina <aferdisoukaina8@gmail.com>
2025-12-14 14:34:48 +0100

    Update TicketItem and db.json

--- src/components/TicketItem.js
+++ src/components/TicketItem.js
@@ -1,26 +1,31 @@
-
 import { calculateHours } from "../utils/timeUtils";
 import { useDispatch } from "react-redux";
-import {resolveTicket}from"../redux/ticketsSlice";
+import { resolveTicket } from "../redux/ticketsSlice";
 
 const TicketItem = ({ ticket }) => {
   const dispatch = useDispatch();
   const hours = calculateHours(ticket.dateCreation);
-  const isLate = hours >= 48 && ticket.status !== "résolu";
+  const isLate = hours >= 48 && ticket.statut !== "résolu";
+
 
   const handleResolve = () => {
     dispatch(resolveTicket(ticket));
   };
 
   return (
-    <div className="card p-3 mb-2 shadow-sm">
-      <h5>{ticket.title}</h5>
+    <div className="card p-3 mb-3 shadow-sm">
+      <h5 className="card-title text-center">{ticket.titre}</h5>
 
-      <span className={`badge ${isLate ? "bg-danger" : "bg-secondary"}`}>
+     
+      <span className={`badge ${isLate ? "bg-danger" : "bg-secondary"} fs-6`}>
         {isLate ? "En retard (48h+)" : `${hours}h`}
       </span>
 
-      <button className="btn btn-success mt-2" onClick={handleResolve}>
+     
+      <button 
+        className="btn btn-success btn-sm mt-2"
+        onClick={handleResolve}
+      >
         Marquer résolu
       </button>
     </div>
